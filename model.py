cba632904c5745f2127118212c9371b9f395e869
Sisi Hu <shu004@gmail.com>
2022-01-25 18:24:19 -0700

    initial commit

---
+++ model.py
@@ -0,0 +1,25 @@
+"""Models for melon tasting app."""
+
+from flask_sqlalchemy import SQLAlchemy
+
+db = SQLAlchemy()
+
+
+# Replace this with your code!
+
+
+def connect_to_db(flask_app, db_uri="postgresql:///tasting", echo=True):
+    flask_app.config["SQLALCHEMY_DATABASE_URI"] = db_uri
+    flask_app.config["SQLALCHEMY_ECHO"] = echo
+    flask_app.config["SQLALCHEMY_TRACK_MODIFICATIONS"] = False
+
+    db.app = flask_app
+    db.init_app(flask_app)
+
+    print("Connected to the db!")
+
+
+if __name__ == "__main__":
+    from server import app
+
+    connect_to_db(app)
