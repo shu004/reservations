eef04e91c3e7c8294c7439c55aec1b3f8a1a3907
Sisi Hu <shu004@gmail.com>
2022-01-25 22:33:50 -0700

    created User and Reservation tables and established relationships

--- model.py
+++ model.py
@@ -4,8 +4,31 @@ from flask_sqlalchemy import SQLAlchemy
 
 db = SQLAlchemy()
 
+class User(db.Model):
+    """User Model"""
 
-# Replace this with your code!
+    __tablename__ = "users"
+
+    user_id = db.Column(db.Integer, primary_key=True, autoincrement=True)
+    username = db.Column(db.String(50), nullable=False, unique=True)
+    password = db.Column(db.String(50), nullable=True)
+
+    #Establishing relationships
+    reservations = db.relationship("Reservation", back_populates="users")
+
+
+class Reservation(db.Model):
+    """Reservation Model"""
+
+    __tablename__ = "reservations"
+
+    res_id = db.Column(db.Integer, primary_key=True, autoincrement=True)
+    user_id = db.Column(db.Integer, db.ForeignKey("users.user_id"), nullable=False)
+    res_date = db.Column(db.DateTime, nullable=False)
+    res_time = db.Column(db.DateTime, nullable=False)
+    
+    #Establishing relationships
+    users = db.relationship("User", back_populates="reservations")
 
 
 def connect_to_db(flask_app, db_uri="postgresql:///tasting", echo=True):
